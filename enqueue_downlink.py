f47aa24d69c53835d75c512a25d050d1cba96460
tenspot <mervyn.posnett@gmail.com>
2021-07-16 21:17:26 +0100

    update commit

---
+++ enqueue_downlink.py
@@ -0,0 +1,50 @@
+"""
+Date: July 2021
+
+Source: https://www.chirpstack.io/application-server/api/python-examples/
+
+Need to set the IP address of the LoRaWAN Gateway server and an api_token from the ChirpStack gateway
+web interface.
+
+
+
+"""
+
+import os
+import sys
+
+import grpc
+from chirpstack_api.as_pb.external import api
+
+# Configuration.
+
+# This must point to the API interface.
+server = "192.168.0.7:8080"
+
+# The DevEUI for which you want to enqueue the downlink.
+dev_eui = bytes([0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01])
+
+# The API token (retrieved using the web-interface).
+api_token = "..."
+
+if __name__ == "__main__":
+  # Connect without using TLS.
+  channel = grpc.insecure_channel(server)
+
+  # Device-queue API client.
+  client = api.DeviceQueueServiceStub(channel)
+
+  # Define the API key meta-data.
+  auth_token = [("authorization", "Bearer %s" % api_token)]
+
+  # Construct request.
+  req = api.EnqueueDeviceQueueItemRequest()
+  req.device_queue_item.confirmed = False
+  req.device_queue_item.data = bytes([0x01, 0x02, 0x03])
+  req.device_queue_item.dev_eui = dev_eui.hex()
+  req.device_queue_item.f_port = 10
+
+  resp = client.Enqueue(req, metadata=auth_token)
+
+  # Print the downlink frame-counter value.
+  print(resp.f_cnt)
